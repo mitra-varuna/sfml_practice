d67c811aecb153587296b4d18178bd3818b4cd7d
Sushant Srivastava <sushant.srivastav@gmail.com>
2016-07-04 04:10:59 +0530

    Made a rectangle

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,15 @@
 #include <SFML/Graphics.hpp>
 
 int main(int argc, char const *argv[]) {
-  sf::RenderWindow window(sf::VideoMode(640, 80), "First Window");
+  sf::RenderWindow window(sf::VideoMode(640, 480), "First Window");
+
+  //Creating a Rectangle
+
+  sf::RectangleShape rectangle(sf::Vector2f(128.0f, 128.0f));
+  rectangle.setFillColor(sf::Color::Red);
+  rectangle.setPosition(320, 240);
+  rectangle.setOrigin(rectangle.getSize().x/2, rectangle.getSize().y/2);
+
   while(window.isOpen()) {
     sf::Event event;
     while(window.pollEvent(event)) {
@@ -10,6 +18,7 @@ int main(int argc, char const *argv[]) {
       }
     }
     window.clear(sf::Color::Black);
+    window.draw(rectangle);
     window.display();
   }
   return 0;
