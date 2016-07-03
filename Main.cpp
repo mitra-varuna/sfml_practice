e5f520568423f98f50794f3bf820346d05f0b955
Sushant Srivastava <sushant.srivastav@gmail.com>
2016-07-04 04:04:01 +0530

    Trying SFML with cmake

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,16 @@
 #include <SFML/Graphics.hpp>
 
 int main(int argc, char const *argv[]) {
-  /* code */
+  sf::RenderWindow window(sf::VideoMode(640, 80), "First Window");
+  while(window.isOpen()) {
+    sf::Event event;
+    while(window.pollEvent(event)) {
+      if(event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+    window.clear(sf::Color::Black);
+    window.display();
+  }
   return 0;
 }
